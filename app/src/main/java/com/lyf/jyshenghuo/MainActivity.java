669e474b1eefc7695b01e1a3e8e1ea868a1a8ea7
lyf <18839901555@139.com>
2020-05-26 23:06:40 +0800

    first.commit

---
+++ app/src/main/java/com/lyf/jyshenghuo/MainActivity.java
@@ -0,0 +1,11 @@
+package com.lyf.jyshenghuo;
+import android.os.Bundle;
+import com.lyf.jyshenghuo.BaseClass.BaseActivity;
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
